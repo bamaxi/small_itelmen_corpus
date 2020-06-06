65c6593525839078d2c844b1e2a952f5121f91e5
Bazhukov Max <oa.bazhukov@gmail.com>
2020-06-07 01:14:15 +0300

    upload

--- app/update_db/start_db.py
+++ app/update_db/start_db.py
@@ -7,12 +7,13 @@ from app.models import Text, Paragraph, Phrase, Word, Morph
 
 from app.update_db.update import add_data
 
+
 BASE_FILE = 'xml_texts/text_1.xml'
 
 
 def add_data_from_file(file=BASE_FILE):
     if file==BASE_FILE:
-        cont = input('Adding base file, continue? (y/n)')
+        cont = input(f'Adding base file ({file}), continue? (y/n)')
         if cont != 'y':
             return
     add_data(file)
