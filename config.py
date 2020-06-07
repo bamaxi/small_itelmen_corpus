e9de092c780eb37605654b264a785b65be53517e
Bazhukov Max <oa.bazhukov@gmail.com>
2020-06-07 22:39:28 +0300

    docs

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     # SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SECRET_KEY = 'crowd_corpus_eleven'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'crowd_corpus_one_two_three'
     # DATABASE = os.path.join(app.instance_path, 'app.sqlite')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
